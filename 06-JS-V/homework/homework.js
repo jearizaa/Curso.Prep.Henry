460749c904eeba9f77da25514dab73a2e0b50698
jearizaa <jearizaa@unal.edu.co>
2020-11-24 18:58:11 -0500

    h5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,14 +8,28 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(opciones) {
+    this.usuario = opciones.usuario
+    this.nombre = opciones.nombre
+    this.email = opciones.email
+    this.password = opciones.password
+  }
 
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es ' + this.nombre
+  }
+
+  return Usuario
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+  Constructor.prototype.saludar = function(){
+    return 'Hello World!'
+  }
+  return Constructor
 }
 
 function agregarStringInvertida() {
@@ -24,7 +38,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  String.prototype.reverse = function(){
+    var inverted = ''
+    for(let i = this.length; i >= 0 ; i--){
+      inverted = inverted + this.charAt(i)
+    }
+    return inverted
+  }
 }
 
 // No modificar nada debajo de esta línea
